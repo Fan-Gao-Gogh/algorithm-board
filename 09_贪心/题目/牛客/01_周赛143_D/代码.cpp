7abc5b606151f00bc2411f5aa1101d665eccb196
Fan-Gao-Gogh <114432748@qq.com>
2026-05-11 01:56:47 +0800

    Update 代码.cpp

--- 09_贪心/题目/牛客/01_周赛143_D/代码.cpp
+++ 09_贪心/题目/牛客/01_周赛143_D/代码.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+typedef long long LL;
+typedef pair<LL, int> PII;
+vector<PII> events;
 
+int main() {
+    LL n, k;
+    cin >> n >> k;
+    for (LL i = 0; i < n; i++) {
+        LL l, r;
+        cin >> l >> r;
+        events.push_back({l - k, 1});   // 左端点：+1
+        events.push_back({r + 1, -1});  // 右端点+1：-1
+    }
+    
+    // 按位置排序，位置相同则-1在前
+    sort(events.begin(), events.end());
+    
+    LL current = 0, ans = 0;
+    for (auto &e : events) {
+        current += e.second;
+        ans = max(ans, current);
+    }
+    
+    cout << ans << endl;
+    return 0;
+}
